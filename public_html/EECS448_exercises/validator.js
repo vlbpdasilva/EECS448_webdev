a1751344f8117daf6f732d298e65f5f36f435c65
Victor Berger <victorluiz.m4@gmail.com>
2016-02-25 23:20:29 -0600

    Finish password validator program

--- public_html/EECS448_exercises/validator.js
+++ public_html/EECS448_exercises/validator.js
@@ -4,14 +4,19 @@ function validator()
 	var pw1 = document.getElementById("form1").elements.namedItem("password1").value;
 	var pw2 = document.getElementById("form1").elements.namedItem("password2").value;
 	
-	if (pw1 == pw2)
+	if (pw1 == pw2 && pw1.length > 7 )
 	{
 		alert("Passwords match.");
 	}
-	else
+	
+	else if (pw1 == pw2 && pw1.length < 8)
+	{
+		alert("Password must be at least 8 characters long.");
+
+	}
+	
+	else if (pw1 != pw2)
 	{
 		alert("Passwords do NOT match");
 	}
-
-
 }
